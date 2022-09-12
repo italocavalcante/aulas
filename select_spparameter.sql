ed995a8a425788aa54c9bd90e1e755866932e40e
italocavalcante <italocavalcantefortaleza@live.com>
2022-09-12 13:22:03 -0300

    Create select_spparameter.sql

---
+++ select_spparameter.sql
@@ -0,0 +1,11 @@
+set lines 200
+set pages 1000
+col name format a25
+col value format a25
+col update_comment format a50
+col sid format a10
+select sid,name,value,update_comment from v$spparameter where name in
+('undo_tablespace','sga_target','sga_max_size','memory_target','memory_max_target',
+'pga_aggregate_target','pga_aggregate_limit','db_name','db_unique_name'
+,'cluster_database','undo_tablespace','log_archive_dest','log_archive_dest_1',
+'db_create_file_dest','db_create_online_log_dest%','db_recovery_file_dest','job_queue_processes') order by name;
